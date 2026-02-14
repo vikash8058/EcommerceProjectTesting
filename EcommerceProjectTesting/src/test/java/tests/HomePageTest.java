f02ca28b1c587ca5efc6b9361452ea75a842a31d
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2026-02-15 00:07:35 +0530

    [Vikash Prajapati] : added test files of ContactUsPage and HomePage

--- EcommerceProjectTesting/src/test/java/tests/HomePageTest.java
+++ EcommerceProjectTesting/src/test/java/tests/HomePageTest.java
@@ -2,14 +2,55 @@ package tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import base.BaseTest;
+import pages.HomePage;
 
 public class HomePageTest extends BaseTest {
 
 	@Test
-	public void verifyTitle() {
+	public void verifyHomePageLoaded() {
 
-		String title = driver.getTitle();
-		Assert.assertTrue(title.contains("4892"));
+		HomePage homePage = new HomePage(driver);
+
+		Assert.assertTrue(homePage.isLogoDisplayed(), "Home page did not load properly");
 	}
+
+	@Test
+	public void verifyScrollUpFeature() {
+
+		HomePage homePage = new HomePage(driver);
+
+		homePage.scrollToFooter();
+		homePage.clickScrollUp();
+
+		Assert.assertTrue(homePage.isTopLogoVisible(), "Scroll up did not work");
+	}
+
+	@Test
+	public void verifyCategorySectionVisible() {
+
+		HomePage homePage = new HomePage(driver);
+
+		Assert.assertTrue(homePage.isCategorySectionDisplayed(), "Category section not visible");
+	}
+
+	@Test
+	public void verifyFeaturedItemsSection() {
+
+		HomePage homePage = new HomePage(driver);
+
+		Assert.assertTrue(homePage.isFeaturedItemsVisible(), "Featured items section missing");
+	}
+
+	@Test
+	public void verifyFooterVisible() {
+
+		HomePage homePage = new HomePage(driver);
+
+		homePage.scrollToFooter();
+
+		Assert.assertTrue(homePage.isFooterVisible(), "Footer not visible");
+	}
+
 }
